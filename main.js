1e50b97548b4890ce41c5163e5c7b46c062dde9c
Gabriela <gabyplasencia1995@gmail.com>
2023-08-30 17:32:26 +0100

    parte del script para validar los input

---
+++ main.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('form');
+const dayI = document.getElementById('day-input');
+const monthI = document.getElementById('month-input');
+const yearI = document.getElementById('year-input');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputWrapper = element.parentElement;
+    const errorDisplay = inputWrapper.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputWrapper.classList.add('error');
+    inputWrapper.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputWrapper = element.parentElement;
+    const errorDisplay = inputWrapper.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputWrapper.classList.add('success');
+    inputWrapper.classList.remove('error');
+};
+
+const validateInputs = () => {
+    const dayValue = dayI.value.trim();
+    const monthValue = monthI.value.trim();
+    const yearValue = yearI.value.trim();
+
+
+    if(dayValue === '') {
+        setError(dayI, 'This field is required');
+    } else {
+        setSuccess(dayI);
+    }
+
+    if(monthValue === '') {
+        setError(monthI, 'This field is required');
+    } else {
+        setSuccess(monthI);
+    }
+
+    if(yearValue === '') {
+        setError(yearI, 'This field is required');
+    } else {
+        setSuccess(yearI);
+    }
+
+};
