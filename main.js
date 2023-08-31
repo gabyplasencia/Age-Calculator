034c332e9019d0010cac532e88520af8b2dff993
Gabriela <gabyplasencia1995@gmail.com>
2023-08-31 12:50:37 +0100

    script para la validacion de los campos y el calculo de la edad, aun tiene fallos

--- main.js
+++ main.js
@@ -6,49 +6,75 @@ const yearI = document.getElementById('year-input');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    validateInputs();
+    validateInputs(dayI, monthI, yearI );
+
 });
 
 const setError = (element, message) => {
+    const formHead = element.closest(".form__head")
     const inputWrapper = element.parentElement;
     const errorDisplay = inputWrapper.querySelector('.error-message');
-
     errorDisplay.innerText = message;
-    inputWrapper.classList.add('error');
-    inputWrapper.classList.remove('success')
+    formHead.classList.add('error');
 }
 
 const setSuccess = element => {
+    const formHead = element.closest(".form__head")
     const inputWrapper = element.parentElement;
     const errorDisplay = inputWrapper.querySelector('.error-message');
-
     errorDisplay.innerText = '';
-    inputWrapper.classList.add('success');
-    inputWrapper.classList.remove('error');
+    formHead.classList.remove('error');
 };
 
-const validateInputs = () => {
-    const dayValue = dayI.value.trim();
-    const monthValue = monthI.value.trim();
-    const yearValue = yearI.value.trim();
+const validateInputs = (dayI, monthI, yearI) => {
+    const currentDate = new Date(); 
+    let success = true;
 
+    const day = dayI.value.trim();
+    const month = monthI.value.trim();
+    const year = yearI.value.trim();
 
-    if(dayValue === '') {
+    const lastDayOfMonth = new Date(year, parseInt(month), 0).getDate();
+    if(day === '') {
         setError(dayI, 'This field is required');
+        success = false;
+    } else if(!parseInt(day) || day <=0  || day > 31 || day > lastDayOfMonth){
+        setError(dayI, 'Must be a valid day');
+        success = false;
     } else {
         setSuccess(dayI);
     }
 
-    if(monthValue === '') {
+    if(month === '') {
         setError(monthI, 'This field is required');
-    } else {
+        success = false;
+    } else if(!parseInt(month) || month <= 0 || month > 12){
+        setError(monthI, 'Must be a valid month');
+        success = false;
+    } else if(success){
         setSuccess(monthI);
     }
 
-    if(yearValue === '') {
+    if(year === '') {
         setError(yearI, 'This field is required');
-    } else {
+        success = false;
+    } else if(!parseInt(year) || year > currentDate.getFullYear()){
+        setError(yearI, 'Must be a valid year');
+        success = false;
+    } else if(success){
         setSuccess(yearI);
     }
 
+    if(!success){
+        return;
+    }
+        const yearAge = currentDate.getFullYear() - year;
+        const monthAge = currentDate.getMonth() - month;
+        const dayAge = currentDate.getDate() - day;
+    
+        document.getElementById('age-year').innerHTML = yearAge;
+        document.getElementById('age-month').innerHTML = monthAge;
+        document.getElementById('age-day').innerHTML = dayAge; 
+    
 };
+
